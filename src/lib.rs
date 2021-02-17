9369bd81161a9e3a36b475b89fd99eafd35d7b07
Keating Reid <keating.reid@protonmail.com>
2021-02-17 13:50:45 -0500

    Basic working implementation

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use syn::{
+    parse_macro_input,
+    LitStr,
+    parse::{Parse, ParseStream, Result},
+};
+use quote::quote;
+use proc_macro::TokenStream;
+
+struct ParseTarget(LitStr);
+
+impl Parse for ParseTarget {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(ParseTarget(input.parse()?))
+    }
+}
+
+#[proc_macro]
+pub fn explode(input: TokenStream) -> TokenStream {
+    let input_str = parse_macro_input!(input as ParseTarget).0.value();
+    let iter = input_str.chars();
+    (quote! { [#(#iter),*] }).into()
+}
